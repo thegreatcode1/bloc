98656757cf5ffc0646464c6504d72ce66045496c
thegreatcode1 <suhaibpa72@gmail.com>
2023-10-17 19:48:57 +0530

    add increment and decrement values process

--- lib/counter_bloc/counter_bloc.dart
+++ lib/counter_bloc/counter_bloc.dart
@@ -6,6 +6,16 @@ part 'counter_state.dart';
 
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
   CounterBloc() : super(Initialstate()) {
-    
+    on<Increment>((event, emit) {
+      final currentstatevalue = state.count;
+      final incrementedvalue = currentstatevalue + 1;
+      return emit(CounterState(count: incrementedvalue));
+    });
+
+    on<Decrement>((event, emit) {
+      final currentstatevalue = state.count;
+      final decrementvalue = currentstatevalue - 1;
+      return emit(CounterState(count: decrementvalue));
+    });
   }
 }
